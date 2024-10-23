d31ec5afd1e6389616b1662de1b6a94a2069afd9
RomanSibakovskij <j4023958@gmail.com>
2024-10-23 07:18:23 +0300

    Add empty RegisterPage sheet

---
+++ src/main/java/com/abantecart/demo/RegisterPage.java
@@ -0,0 +1,16 @@
+package com.abantecart.demo;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+import java.util.*;
+
+public class RegisterPage extends BasePage{
+
+    public RegisterPage(WebDriver driver) {
+        super(driver);
+    }
+    
+}
